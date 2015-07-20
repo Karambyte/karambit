d0eccc9facfa6b5277b00a6dd7c29dceb98d8a28
Alan Doherty <alan.fred.doherty@googlemail.com>
2015-07-20 16:32:22 +0100

    Object serializer work

--- Karambit/Serialization/ObjectSerializer.cs
+++ Karambit/Serialization/ObjectSerializer.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿using Karambit.IO;
+using System;
 
 namespace Karambit.Serialization
 {
-    class ObjectSerializer
+    public class ObjectSerializer : Serializer
     {
+        #region Enums
+        enum 
+        #endregion
+
+        #region Methods
+        public override T Deserialize<T>(System.IO.Stream stream) {
+            // read type
+            byte typeCode = (byte)stream.ReadByte();
+
+            TypeCode.
+
+            ObjectReader reader = new ObjectReader();
+        }
+
+        public override void Serialize<T>(System.IO.Stream stream, T obj) {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanDeserialize {
+            get {
+                return true;
+            }
+        }
+
+        public override bool CanSerialize {
+            get {
+                return true;
+            }
+        }
+        #endregion
     }
 }
