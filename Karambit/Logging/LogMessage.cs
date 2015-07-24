2ac0be58dcea60cd1a8a2d0377c48082ba0a1470
Alan Doherty <alan.fred.doherty@googlemail.com>
2015-07-25 01:53:28 +0300

    Added subscription and blocking to logging

---
+++ Karambit/Logging/LogMessage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Karambit.Logging
+{
+    public class LogMessage
+    {
+        #region Fields
+        public string message;
+        public LogLevel level;
+        public string channel;
+        #endregion
+
+        #region Properties        
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        /// <value>The message.</value>
+        public string Message {
+            get {
+                return message;
+            }
+        }
+
+        /// <summary>
+        /// Gets the level.
+        /// </summary>
+        /// <value>The level.</value>
+        public LogLevel Level {
+            get {
+                return level;
+            }
+        }
+
+        /// <summary>
+        /// Gets the channel.
+        /// </summary>
+        /// <value>The channel.</value>
+        public string Channel {
+            get {
+                return channel;
+            }
+        }
+        #endregion
+
+        #region Constructors        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogMessage"/> class.
+        /// </summary>
+        /// <param name="level">The level.</param>
+        /// <param name="channel">The channel.</param>
+        /// <param name="message">The message.</param>
+        public LogMessage(LogLevel level, string channel, string message) {
+            this.level = level;
+            this.channel = channel;
+            this.message = message;
+        }
+        #endregion
+    }
+}
--- Karambit/Logging/Logger.cs
+++ Karambit/Logging/Logger.cs
@@ -1,16 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Karambit.Logging
 {
-    public class Logger
+    public class Logger : IObservable<LogMessage>
     {
+        #region Fields
+        private List<string> blockedChannels = new List<string>();
+        private List<IObserver<LogMessage>> observers = new List<IObserver<LogMessage>>();
+        #endregion
+
         #region Methods        
+        /// <summary>
+        /// Blocks the specified channel from emitting log messages.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        public void Block(string channel) {
+            if (!blockedChannels.Contains(channel))
+                this.blockedChannels.Add(channel);
+        }
+
+        /// <summary>
+        /// Unblocks the specified channel from emitting log messages.
+        /// </summary>
+        /// <param name="channel">The channel.</param>
+        public void Unblock(string channel) {
+            if (blockedChannels.Contains(channel))
+                blockedChannels.Remove(channel);
+        }
+
         /// <summary>
         /// Logs a message to the specified channel.
         /// </summary>
         /// <param name="channel">The channel.</param>
         /// <param name="msg">The message.</param>
         public void Log(LogLevel level, string channel, string msg) {
+            // check if blocked
+            if (blockedChannels.Contains(channel))
+                return;
+
+            // build
             string str = "[" + channel + "] " + msg;
 
             // write to console
@@ -18,6 +47,22 @@ namespace Karambit.Logging
                 WriteLineC(ConsoleColor.Red, str);
             else
                 Console.WriteLine(str);
+
+            // push to observers
+            LogMessage message = new LogMessage(level, channel, msg);
+
+            foreach (IObserver<LogMessage> observer in observers)
+                observer.OnNext(message);
+        }
+
+        /// <summary>
+        /// Subscribes the specified observer.
+        /// </summary>
+        /// <param name="observer">The observer.</param>
+        /// <returns></returns>
+        public IDisposable Subscribe(IObserver<LogMessage> observer) {
+            observers.Add(observer);
+            return null;
         }
 
         /// <summary>
